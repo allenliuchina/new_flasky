35f0868e1d6ed4173648e07379ec347c858825c9
lya <liuyaowa@gmail.com>
2018-08-02 15:03:44 +0800

    charpeter8

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from config import config
+from flask_login import LoginManager
+from flask_mail import Mail
 
+mail = Mail()
 bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -15,7 +21,11 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     moment.init_app(app)
+    mail.init_app(app)
+    login_manager.init_app(app)
     from .main import main
     app.register_blueprint(main)
+    from .auth import auth
+    app.register_blueprint(auth)
 
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -17,19 +17,3 @@ def index():
         session['name'] = form.name.data
         return redirect(url_for('.index'))
     return render_template('index.html', name=session.get('name'), form=form, current_time=current_time)
-
-
-@main.route('/register', methods=['POST', 'GET'])
-def register():
-    form = NameForm()
-    if form.validate_on_submit() and not User.query.filter_by(username=form.name.data).first():
-        user = User(username=form.name.data, role_id=3)
-        db.session.add(user)
-        return 'ok'
-    form.name.data = ''
-    return render_template('register.html', form=form)
-
-
-@main.route('/<name>')
-def user_id(name):
-    return render_template('user.html', name=name)
