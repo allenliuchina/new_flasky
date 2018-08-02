32fcb3f62c36c12625fc42c18ab9d55881c33534
lya <liuyaowa@gmail.com>
2018-08-02 16:30:12 +0800

    charpter 8 end

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+
+
 class Config():
     SECRET_KEY = 'DJFLDSFJKDSJFJ'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
@@ -7,11 +10,20 @@ class Config():
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:root@127.0.0.1/test'
-#    SQLALCHEMY_DATABASE_URI = 'sqlite:////home/lya/doc/new_flasky/test.sqlite'
-    TESTING=True
+    #    SQLALCHEMY_DATABASE_URI = 'sqlite:////home/lya/doc/new_flasky/test.sqlite'
+    TESTING = True
+
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:root@127.0.0.1/dev'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.qq.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+                   ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <772075034@qq.com>'
 
 
 config = {
